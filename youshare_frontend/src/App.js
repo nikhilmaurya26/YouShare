8f94427cb2c4ee0f5bad4f1f18ad995f7f435db0
Nikhil <nikhilmaurya0926@gmail.com>
2023-08-21 00:02:38 +0530

    Subject - Login page -setup the routes -setup the dependencies

--- youshare_frontend/src/App.js
+++ youshare_frontend/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      Hello wprld.
-    </div>
-  )
+    <GoogleOAuthProvider clientId='748428699897-1na02su3dt4dgv40f35aio88ea0co3b7.apps.googleusercontent.com'>
+      <Routes>
+        <Route path='login' element={<Login />} />
+        <Route path='/*' element={<Home />} />
+      </Routes>
+    </GoogleOAuthProvider>
+  );
 }
 
-export default App
+export default App;
